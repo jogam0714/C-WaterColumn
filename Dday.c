f757ec4ccd7fe9314924ed4ff2fac9d7d62278d1
jogam0714 <seunghwahan67@gmail.com>
2026-06-25 15:03:19 +0900

    Update Dday.c

--- Dday.c
+++ Dday.c
@@ -1,31 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-
-int month[3];
-int day[3];
-
-int main(int argc, char const *argv[])
+void Dday()
 {
-    month[0]=7;
-    day[0]=3;
-    Dday;
-    return 0;
-}
-
-
-
-void Dday(int t_month, int t_day, int Dday)
-{
-    
-    printf("오늘 날짜를 입력하세요");
-    Dday=0;
+    int t_month, t_day;
+    printf("오늘 날짜를 입력하세요(월 일): ");
+    int Dday = 0;
+    int sumM = 0;
+    int sumTM = 0;
     scanf("%d %d", &t_month, &t_day);
-    int days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
-
-    for (int i=0; i<sizeof(month); i++){
-        Dday = (days[month[i]-1]-days[t_month-1])+(day[i]-t_day);
-        printf("%d\n", Dday);
+    for (int i = 0; i < total; i++)
+    {
+        for (int j = 0; j < month[i] - 1; j++)
+        {
+            sumM += days[j]; // 1.1부터 수행일까지 일수
+        }
+        for (int j = 0; j < t_month - 1; j++)
+        {
+            sumTM += days[j]; // 1.1부터 오늘까지 일수
+        }
+        Dday = sumM + day[i] - (sumTM + t_day); // 오늘부터 수행일까지 남은 기간
+        printf("%sD-%d\n", exam_name[i], Dday);
+        Dday = 0;   //디데이 초기화 
+        sumM = 0;
+        sumTM = 0;
     }
 }
-
-
