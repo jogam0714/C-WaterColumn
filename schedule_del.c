c5f863178665cb1ec1822f4ff73eb600e36266ec
jogam0714 <seunghwahan67@gmail.com>
2026-06-25 15:04:35 +0900

    Refactor schedule deletion logic and improve prompts

--- schedule_del.c
+++ schedule_del.c
@@ -1,39 +1,74 @@
+void schedule_del()
+{
+    int del = 0;
+    if (total == 0) //추가된 일정 수==0
+    {
+        printf("등록된 수행평가가 없습니다.");
+        return;
+    }
 
-
-
-#define MAX 50
-int month[MAX];
-int day[MAX];
-int period[MAX];
-char subject[MAX][30];
-char exam_name[MAX][30];
-int total = 0;
-
-
-void schedule_del(){
-    int del;
     printf("삭제할 수행평가를 선택해주세요\n");
+    printf("==================\n");
     for (int i = 0; i < total; i++)
     {
-        printf("%d.%s\n",i+1,exam_name[i]);
+        printf("%d.%s\n", i + 1, exam_name[i]);
     }
-    scanf("%d",&del);
-    if(del-1 == 0)
+    printf("==================\n");
+    while (1)   //무한 반복
     {
-        month[del-1] = 
-        day[i] = day[i + 1];
-        period[i] = period[i + 1];
-        subject[i] = subject[i + 1];
-        exam_name[i] = exam_name[i + 1]
+        if (scanf("%d", &del) == 1)
+        {
+            if (del >= 0 && del <= total)
+            {
+
+                for (int i = del - 1; i < total; i++)
+                {
+                    if (i == total)
+                    {
+                        month[i] = '\0';
+                        day[i] = '\0';
+                        period[i] = '\0';
+                        strcpy(subject[i], '\0');
+                        strcpy(exam_name[i], '\0');
+                    }
+                    else
+                    {
+                        month[i] = month[i + 1];
+                        day[i] = day[i + 1];
+                        period[i] = period[i + 1];
+                        strcpy(subject[i], subject[i + 1]);
+                        strcpy(exam_name[i], exam_name[i + 1]);
+                    }
+                }
+                total--;
+                break;
+            }
+            else
+            {
+                printf("잘못된 입력입니다.\n");
+                printf("삭제할 수행평가를 선택해주세요\n");
+                printf("==================\n");
+                for (int i = 0; i < total; i++)
+                {
+                    printf("%d.%s\n", i + 1, exam_name[i]);
+                }
+                printf("==================\n");
+                while (getchar() != '\n')
+                    ;
+            }
+        }
+        else
+        {
+            printf("잘못된 입력입니다.\n");
+            printf("삭제할 수행평가를 선택해주세요\n");
+            printf("==================\n");
+            for (int i = 0; i < total; i++)
+            {
+                printf("%d.%s\n", i + 1, exam_name[i]);
+            }
+            printf("==================\n");
+            while (getchar() != '\n')
+                ;
+        }
     }
-    for (int i = del-1; i < total; i++)
-    {
-        month[i] = month[i + 1];
-        day[i] = day[i + 1];
-        period[i] = period[i + 1];
-        subject[i] = subject[i + 1];
-        exam_name[i] = exam_name[i + 1]
-    }
-    
-    
 }
